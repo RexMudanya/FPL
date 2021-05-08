996149a24311e2864568dea7aeffadb6c061b439
RexMudanya <musungumudanya@gmail.com>
2021-05-08 12:22:25 +0300

    completed class methods implementation

--- api/player.py
+++ api/player.py
@@ -25,11 +25,57 @@ class Player:
 		gets all data for a specified game week
 		'element', 'fixture', 'opponent_team', 'total_points', 'was_home', 'kickoff_time',
 		'team_h_score', 'team_a_score', 'round', 'minutes', 'goals_scored',
-		'assists', 'clean_sheets', 'goals_conceded', 'own_goals', 'penalties_saved', 'penalties_missed', 'yellow_cards',
+		'assists', 'clean_sheets', 'goals_conceded', 'own_goals', 'penalties_saved', 'penalties_missed',
+		'yellow_cards',
 		'red_cards', 'saves', 'bonus', 'bps', 'influence', 'creativity', 'threat', 'ict_index', 'value',
 		'transfers_balance', 'selected', 'transfers_in', 'transfers_out'
 
+	get_fixtures(int) -> list
+		gets players fixtures
+		returns a list of dicts containing keys:
+		id
+		code
+		team_h
+		team_h_score
+		team_a
+		team_a_score
+		event
+		finished
+		minutes
+		provisional_start_time
+		kickoff_time
+		event_name
+		is_home
+		difficulty
+
+	get_past_history(int) -> list
+		returns a players past seasons history as a dict in a list
+		with the following keys:
+		season_name
+		element_code
+		start_cost
+		end_cost
+		total_points
+		minutes
+		goals_scored
+		assists
+		clean_sheets
+		goals_conceded
+		own_goals
+		penalties_saved
+		penalties_missed
+		yellow_cards
+		red_cards
+		saves
+		bonus
+		bps
+		influence
+		creativity
+		threat
+		ict_index
+
 	"""
+
 	def __init__(self, url=None):
 		"""
 		creates class instance
@@ -60,17 +106,31 @@ class Player:
 		"""
 		return self.get_all_data(player_id)["history"][int(game_week - 1)]
 
-	def get_fixtures(self):
-		pass
+	def get_fixtures(self, player_id):
+		"""
+		returns players fixtures
+		:param player_id: element id
+		:type player_id: int
+		:return: fixtures in a list of dicts
+		:rtype: list
+		"""
+		return self.get_all_data(player_id)["fixtures"]
 
-	def get_past_history(self):
-		pass
+	def get_past_history(self, player_id):
+		"""
+		returns players past history
+		:param player_id: element id
+		:return: past seasons history
+		:rtype: list
+		"""
+		return self.get_all_data(player_id)["history_past"]
 
 
 class Viz:
 	"""
 	methods to plot various player data
 	"""
+
 	def __init__(self, url=None):
 		self.player = Player(url)
 
