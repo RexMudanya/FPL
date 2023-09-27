80f4d2155151dd7f94ce23000f94e9918fa27921
RexMudanya <rexmusungu@lima.ag>
2023-09-27 12:17:59 +0300

    feat: send api request, formatting

--- src/streamlitapp/app.py
+++ src/streamlitapp/app.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 import streamlit as st
 
 # todo: send input as api request
@@ -7,36 +10,31 @@ import streamlit as st
 
 
 player_positions = {
-    'GKP': [0, 0, 1, 0],
-    'DEF': [1, 0, 0, 0],
-    'MID': [0, 0, 0, 1],
-    'FWD': [0, 1, 0, 0]
+    "GKP": [0, 0, 1, 0],
+    "DEF": [1, 0, 0, 0],
+    "MID": [0, 0, 0, 1],
+    "FWD": [0, 1, 0, 0],
 }  # todo: ref, impl dict vectorizer
 
-match = {
-    "home": 1,
-    "away": 0
-}
+match = {"home": 1, "away": 0}
 
 
 def send_request(input: list):  # todo: impl send request
-    if input:
-        return sum(input)
+    return requests.post(
+        url="http://localhost:8008/predict",  # todo: ref to use env variables
+        data=json.dumps({"input": input}),
+    )
 
 
 player_position_selector = st.selectbox(
-    'FPL Player Position',
-    tuple(player_positions.keys())
+    "FPL Player Position", tuple(player_positions.keys())
 )
 player_position = player_positions[player_position_selector]
 
 player_value_input = st.number_input("FPL player value")  # todo: set min, max value
 player_value = player_value_input * 10  # note: may change based on model, ref impl
 
-home_away_input = st.selectbox(
-    "match location",
-    tuple(match.keys())
-)
+home_away_input = st.selectbox("match location", tuple(match.keys()))
 home_away = match[home_away_input]
 
 player_data = player_position
@@ -44,8 +42,7 @@ player_data = player_position
 player_data.append(player_value)
 player_data.append(home_away)
 
-if st.button("Ok", type='primary'):
+if st.button("Ok", type="primary"):
     prediction = send_request(player_data)
     if prediction:
-        st.caption(f':blue[predicted points: {prediction}]')
-
+        st.caption(f":blue[FPL projected points: {prediction.json()['forecast']}]")
