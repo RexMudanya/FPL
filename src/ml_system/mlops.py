1d7ed5f6e3d1ff6a4be147d75fc86db727e9bdbf
RexMudanya <musungumudanya@gmail.com>
2023-11-28 17:07:54 +0300

    wip: migrate to mlflow

--- src/ml_system/mlops.py
+++ src/ml_system/mlops.py
@@ -1,27 +1,37 @@
-import wandb
+import mlflow
+
+config = {
+    "MLFLOW_TRACKING_URI": "sqlite:///mlflow.db",
+    "ENTITY": "",
+    "NAME": "",
+    "ARTIFACT_PATH": "",
+}  # todo: setup in env
 
 
-class Mlops:
-    def __init__(self, project, entity, job, name) -> None:
-        self.run = wandb.init(project=project, entity=entity, job_type=job, name=name)
+class MlflowOps:
+    def __int__(
+        self, experiment: str, config: dict = config
+    ):  # todo: setup config dict
+        assert config is not None, "provide config"
+        assert experiment is not None, "provide experiment name"
 
-    def log_dataset(self, data, name):
-        self.run.job_type = "dataset"
+        self.config = config
+        self.experiment_name = experiment
 
-        artifact = wandb.Artifact(name=name + f"_{self.run.id}", type="training data")
-        artifact.add_file(data)
-        self.run.log_artifact(artifact)
+        mlflow.set_tracking_uri(self.config["MLFLOW_TRACKING_URI"])
+        mlflow.set_experiment(self.experiment_name)
+        mlflow.set_tag(config["entity"], config["name"])
 
-    def log_model(self, model, name):
-        self.run.job_type = "save"
+    def log_training(
+        self, data: tuple[str, str], params: dict, metrics: dict, model: tuple[str, any]
+    ):
+        with mlflow.start_run():
+            mlflow.log_param("train-data", data[0])
+            mlflow.log_param("validation-data", data[1])
+            [mlflow.log_param(k, v) for k, v in params.items() if params]
+            [mlflow.log_metrics(k, v) for k, v in metrics.items() if metrics]
+            mlflow.sklearn.log_model(
+                model[1], model[0], artifact_path=self.config["ARTIFACT_PATH"]
+            )
 
-        artifact = wandb.Artifact(name=name + f"_{self.run.id}", type="model")
-        artifact.add_file(model)
-        self.run.log_artifact(artifact)
-
-        self.run.link_artifact(artifact, "model-registry")
-
-    def log_metrics(self, metrics):
-        self.run.job_type = "evaluate"
-
-        wandb.log({"metrics": metrics})
+        mlflow.end_run()
