5a01bfe141388005e85ad17cbd32a72d40a7ffa0
RexMudanya <musungumudanya@gmail.com>
2023-12-18 20:22:39 +0300

    feat: s3 downloads

--- src/ml_system/cloud_ops.py
+++ src/ml_system/cloud_ops.py
@@ -1,3 +1,4 @@
+import os
 from os.path import basename, exists
 
 import boto3
@@ -9,9 +10,16 @@ class AwsOps:
             aws_access_key_id=access_key, aws_secret_access_key=secret_key
         )
         self.s3 = self.session.resource("s3")
+        self.s3_client = boto3.client("s3")
 
     def upload_s3(self, bucket: str, file, key=None):
         assert exists(file), f"{file} not found"
         self.s3.meta.client.upload_file(
             Filename=file, Bucket=bucket, key=key if key else basename(file)
         )  # todo: check for success
+        # TODO: ref to client
+
+    def download_s3(self, bucket, file, destination):
+        os.makedirs(os.path.split(destination)[0], exist_ok=True)
+        self.s3_client.download_file(bucket, file, destination)
+        # TODO: test, ref
