0a74579b33410f560468619e8b1a237c9bfe9856
RexMudanya <rexmusungu@lima.ag>
2024-01-17 15:51:55 +0300

    ref: path to data

--- src/api/model.py
+++ src/api/model.py
@@ -1,8 +1,6 @@
 import datetime
 import sys
-from os import makedirs
 from os.path import exists, join
-from pathlib import Path
 
 import joblib
 from loguru import logger
@@ -14,16 +12,14 @@ from ml_system.cloud_ops import AwsOps  # noqa E402
 from ml_system.trainer import Trainer  # noqa E402
 from utils.config import get_config  # noqa E402
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent
 TODAY = datetime.date.today()
 CONFIG = get_config()
 AWS_CLIENT = AwsOps(CONFIG["aws"]["access_key"], CONFIG["aws"]["secret_key"])
 
 
 def train():
-    makedirs(join(str(BASE_DIR), "temp"), exist_ok=True)
     model_trainer = Trainer(
-        join(BASE_DIR, "temp"),  # todo: ref
+        CONFIG["training_data"]["data_location"],
         CONFIG["training_data"]["git_repo_url"],
         CONFIG["training_data"]["training_season"],
         None,  # todo: ref
