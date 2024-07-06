1cbcd671650f41d271397ed6d599f83efc56bbaf
RexMudanya <musungumudanya@gmail.com>
2024-07-06 12:18:31 +0300

    feat: player comparison graphs #major

--- src/streamlitapp/app.py
+++ src/streamlitapp/app.py
@@ -1,10 +1,10 @@
 import sys
-from random import sample
+from statistics import mean
 
 import plotly.graph_objects as go
 import streamlit as st
+
 from data.data import Data
-from plotly.subplots import make_subplots
 
 sys.path.insert(0, "..")
 from utils.config import get_config
@@ -21,7 +21,7 @@ gw_points_ownership = data.get_game_week_points_ownership(gw)[
 gw_xg_xa = data.get_game_week_xg_xa(gw)["gw_xg_xa"][:5]
 
 player_list = data.get_players()
-random_players = sample(player_list["players"], 2)
+random_players = ["Eberechi Eze", "Cole Palmer"]  # TODO: ref with top 2 players
 
 # Streamlit App
 st.title("Fantasy Premier League Dashboard")
@@ -115,18 +115,107 @@ elif choice == "Player Comparison":
         "Select Players to Compare", player_list["players"], default=random_players
     )
 
+    points_comparison = go.Figure()
+    points_per_90_comparison = go.Figure()
+    expected_goals_comparison = go.Figure()
+    expected_assists_comparison = go.Figure()
+
     if players_selected:
-        stats = {name: {} for name in players_selected}
+        # stats = {name: {} for name in players_selected}
         for player in players_selected:
-            stats[player]["points"] = data.get_player_points_per_game_week(player)[
+            points_per_gw = data.get_player_points_per_game_week(player)[
                 "points_per_gw"
             ]
-            stats[player]["points_ownership"] = data.get_player_90_points_ownership(
-                player
-            )["points_ownership"]
-            stats[player]["player_xg_xa"] = data.get_player_xq_xa(player)[
-                "player_xg_xa"
+            points_ownership_per_gw = data.get_player_90_points_ownership(player)[
+                "points_ownership"
             ]
-        # TODO: plot comparison graphs
+            player_xg_xa_per_gw = data.get_player_xq_xa(player)["player_xg_xa"]
+
+            game_weeks = [entry["GW"] for entry in points_per_gw]
+            total_points = [entry["total_points"] for entry in points_per_gw]
+            # add a trace for each player
+            points_comparison.add_trace(
+                go.Scatter(
+                    x=game_weeks,
+                    y=total_points,
+                    mode="lines",
+                    name=player,
+                    hovertemplate="GW=%{x}<br>total points=%{y}<extra></extra>",
+                )
+            )
+
+            points_per_90_comparison.add_trace(
+                go.Bar(
+                    x=[player],
+                    y=[
+                        mean(
+                            [
+                                entry["points_per_90"]
+                                for entry in points_ownership_per_gw
+                            ]
+                        )
+                    ],
+                    name=player,
+                    hovertemplate="Aggregated Points per 90=%{y}<extra></extra>",
+                )
+            )
+
+            game_weeks_expected = [entry["GW"] for entry in player_xg_xa_per_gw]
+            player_expected_goals = [
+                entry["expected_goals"] for entry in player_xg_xa_per_gw
+            ]
+            player_expected_assists = [
+                entry["expected_assists"] for entry in player_xg_xa_per_gw
+            ]
+
+            expected_goals_comparison.add_trace(
+                go.Bar(
+                    x=game_weeks_expected,
+                    y=player_expected_goals,
+                    name=player,
+                    hovertemplate="GW=%{x}<br>xG=%{y}<extra></extra>",
+                )
+            )
+            expected_assists_comparison.add_trace(
+                go.Bar(
+                    x=game_weeks_expected,
+                    y=player_expected_assists,
+                    name=player,
+                    hovertemplate="GW=%{x}<br>xA=%{y}<extra></extra>",
+                )
+            )
+
+        points_comparison.update_layout(
+            title="Points per Game week",
+            xaxis_title="Game Week (GW)",
+            yaxis_title="Total points",
+            legend_title="Players",
+        )
+        points_per_90_comparison.update_layout(
+            title="Aggregated Points per 90",
+            xaxis_title="Game Week(GW)",
+            yaxis_title="Aggregated Points Per 90 minutes",
+            # barmode="group",
+            showlegend=False,
+        )
+        expected_goals_comparison.update_layout(
+            title="Player expected Goals per Game week",
+            xaxis_title="Game Week(GW)",
+            yaxis_title="expected goals (xG)",
+            barmode="group",
+            legend_title="Players",
+        )
+        expected_assists_comparison.update_layout(
+            title="Player expected Assists per Game week",
+            xaxis_title="Game Week(GW)",
+            yaxis_title="expected assists (xA)",
+            barmode="group",
+            legend_title="Players",
+        )
+
+        st.plotly_chart(points_comparison)
+        st.plotly_chart(points_per_90_comparison)
+        st.plotly_chart(expected_goals_comparison)
+        st.plotly_chart(expected_assists_comparison)
     else:
         st.warning("Please select at least one player to compare")
